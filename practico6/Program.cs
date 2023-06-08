82fdba421d3a0adbb2809f5a32d6aa099d3ac9c2
777may <mayri.luli@gmail.com>
2023-06-08 18:24:47 -0300

    Cambios antes de probar pt4

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -10,20 +10,29 @@ Console.WriteLine("valor de b: " + b);
 // Console.ReadKey(); espera una tecla
 // Console.WriteLine("txt"); muestra por pantalla
 
-int x, aux = 0;
-Console.WriteLine("Ingrese un numero a invertir: ");
-bool control = int.TryParse(Console.ReadLine(), out x);
-if (control){
-    Console.WriteLine("Valor a invertir: " + x);
-    while (x>0){
-        aux += x%10;
-        x /= 10;
-        aux *= 10;
-    }
-    aux /= 10;
-    Console.WriteLine("Valor invertido: " + aux);
-}else{
-    Console.WriteLine("No ha ingresado un num");
-}
 
+// int x, aux = 0;
+// Console.WriteLine("Ingrese un numero a invertir: ");
+// bool control = int.TryParse(Console.ReadLine(), out x);
+// if (control){
+//     Console.WriteLine("Valor a invertir: " + x);
+//     while (x>0){
+//         aux += x%10;
+//         x /= 10;
+//         aux *= 10;
+//     }
+//     aux /= 10;
+//     Console.WriteLine("Valor invertido: " + aux);
+// }else{
+//     Console.WriteLine("No ha ingresado un num");
+// }
 
+string? texto1, texto2;
+string texto3;
+Console.WriteLine("Ingrese una oracion: ");
+texto1 = Console.ReadLine();
+Console.WriteLine($"La longitud de la oracion {texto1} es: {(texto1 != null ? texto1.Length : 0)}.");
+Console.WriteLine("Ingrese otra oracion: ");
+texto2 = Console.ReadLine();
+texto1 += texto2;
+Console.WriteLine($"La nueva cadena es: {texto1}.");
---
+++ readme.md
@@ -0,0 +1,20 @@
+#Pto 4:
+String es tipo por referencia 
+Tiene las secuencias de escape: 
+    \'
+    \"
+    \\
+    \0
+    \a
+    \b
+    \f
+    \n
+    \r
+    \t
+    \v
+    \u
+    \U
+    \x
+Cuando se usa @ se escribe de forma literal, evita tener que usar \ antes de otro \ pero también hace que debamos usar " antes de otra "
+Cuando se usa $ se escribe de forma interpolada, para poder escribir las variables o fragmentos de código directamente entre {} dentro del texto a imprimir.
+Ambos se pueden combinar escribiendo @$ o $@ indistintamente
